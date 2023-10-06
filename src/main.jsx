6999425ab93a25237eab656f04d5bab0d1b152e2
DeniCosta <denisecosta.codes@gmail.com>
2023-10-05 23:04:03 -0300

    feat: Criação da estlização global

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import GlobalStyle from './styles/GlobalStyle'
 import App from './App.jsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
 )
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  text-decoration: none;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  }
+`;
+
+export default GlobalStyle;
